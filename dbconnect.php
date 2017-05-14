780204c963579cf5d7d3081edddc82220f49b4d5
byongyy <b.yongyy@gmail.com>
2017-05-14 01:11:45 -0500

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
     $host = ":/cloudsql/payment-calculator:us-central1:iou-db";
 	$user = "root";
     $pwd = "password"; 
-    $dbname = "iou-db";
+    $dbname = "iou";
 
     $conn = mysqli_connect($host,$user,$pwd,$dbname);
 
